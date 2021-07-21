7ab2f78681c37e10b84482490f849c159ae79ba2
Sahil Dholakiya <69747994+sahil1710@users.noreply.github.com>
2021-07-21 22:28:07 +0530

    Create BST_implementation.py

---
+++ DS/Trees/Binary tree/search tree/PYTHON/BST_implementation.py
@@ -0,0 +1,47 @@
+class BstNode:
+    def __init__(self, num):
+        self.data = num
+        self.left = None
+        self.right = None
+
+
+def Insert(pointer, data):
+    if pointer is None:
+        pointer = BstNode(data)
+
+    elif data <= pointer.data:
+        pointer.left = Insert(pointer.left, data)
+
+    else:
+        pointer.right = Insert(pointer.right, data)
+
+    return pointer
+
+
+def Search(pointer, data):
+    if pointer is None:
+        return False
+    if data == pointer.data:
+        return True
+
+    elif data < pointer.data:
+        return Search(pointer.left, data)
+
+    else:
+        return Search(pointer.right, data)
+
+if __name__ == "__main__":
+
+    root = None
+
+    root = Insert(root, 10)
+    root = Insert(root, 8)
+    root = Insert(root, 9)
+    root = Insert(root, 7)
+    root = Insert(root, 25)
+    root = Insert(root, 30)
+    root = Insert(root, 15)
+
+    num = int(input("Enter a number: "))
+
+    print("Found" if Search(root, num) else "Not found")
