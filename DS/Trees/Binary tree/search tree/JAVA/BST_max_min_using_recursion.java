f28ac1490d07049cc5edaac6cd0dab2a38d42bee
Sahil Dholakiya <69747994+sahil1710@users.noreply.github.com>
2021-07-24 21:26:42 +0530

    Create BST_max_min_using_recursion.java

---
+++ DS/Trees/Binary tree/search tree/JAVA/BST_max_min_using_recursion.java
@@ -0,0 +1,80 @@
+//node class
+class bstNode{
+    int data;
+    bstNode left;
+    bstNode right;
+
+    // Constructor to create a new node
+    bstNode(int num){
+        data = num;
+        left = null;
+        right = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        bstNode root = null;
+
+        root = Insert(root, 10);
+        root = Insert(root, 8);
+        root = Insert(root, 9);
+        root = Insert(root, 7);
+        root = Insert(root, 25);
+        root = Insert(root, 30);
+        root = Insert(root, 15);
+
+        System.out.println("MAX: " + findMax(root));
+        System.out.println("MIN: " + findMin(root));
+    }
+
+    // insert function
+
+    public static bstNode Insert(bstNode pointer, int data){
+        if(pointer == null){
+            pointer = new bstNode(data);
+        }
+        else if(data<=pointer.data){
+            pointer.left = Insert(pointer.left, data);
+        }
+        else{
+            pointer.right = Insert(pointer.right, data);
+        }
+        return pointer;
+    }
+
+    // function to find max integer in BST
+
+    public static int findMax(bstNode root){
+       if(root==null){
+           System.out.println("Error: tree is empty");
+           return 0;
+       }
+       else if(root.right == null)
+       {
+           return root.data;
+       }
+
+       return findMax(root.right);
+    }
+
+    // function to find min integer in BST
+
+    public static int findMin(bstNode root){
+        if(root==null){
+            System.out.println("Error: tree is empty");
+            return 0;
+        }
+
+        else if(root.left == null)
+        {
+            return root.data;
+        }
+
+        return findMin(root.left);
+    }
+}
+
+// output:
+// MAX: 30
+// MIN: 7
